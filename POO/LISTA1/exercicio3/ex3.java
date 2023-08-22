bb2a389acb28ac628045806ea5085e2a8723a201
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-08-22 19:54:32 -0300

    ex3

--- POO/LISTA1/exercicio3/ex3.java
+++ POO/LISTA1/exercicio3/ex3.java
@@ -1,5 +1,78 @@
 package POO.LISTA1.exercicio3;
+import javax.swing.JOptionPane;
+
+import POO.LISTA1.exericio2.Aluno;
 
 public class ex3 {
-    
+     public static void main(String[] args) {
+    String nome, item;
+    float valorHora, qntdHora, salario, valorDesconto, valorReceber;
+    int cracha;
+    int tipoVinculo; //deveria ser char H ou N
+    Funcionario funcionario = null;
+
+        do{
+            item = JOptionPane.showInputDialog("1- Criar funcionário\n2- Mostrar folha de pagamento\n3- Alterar remuneração\n4- Sair");
+            if (item == null) {
+                System.exit(0);    
+        }
+
+        switch(item){
+            case "1":
+            //cadastrar funcionário
+               cracha = JOptionPane.showInputDialog("Cracha:");
+               nome = JOptionPane.showInputDialog("Nome:");
+               valorDesconto = JOptionPane.showInputDialog("Valor desconto:");
+               
+               //VÍNCULO pode ser H ou N
+               tipoVinculo = JOptionPane.showInputDialog("Tipo vínculo:");
+               
+               //IF - H – horista (entra com horas trabalhadas e valor hora para calcular o salário)
+               valorHora = JOptionPane.showInputDialog("Valor hora:");
+               qntdHora = JOptionPane.showInputDialog("Quantidade hora:");
+               //IF - N – normal (entra apenas com salário
+               salario = JOptionPane.showInputDialog("Salário:");
+               
+
+               funcionario = new Funcionario();
+               funcionario.setcracha(cracha);
+               funcionario.setnome(nome);
+               funcionario.settipoVinculo(tipoVinculo);
+               funcionario.setvalorHora(valorHora);
+               funcionario.setqntdHora(qntdHora);
+               funcionario.setsalario(salario);
+               funcionario.setvalorDesconto(valorDesconto);
+               
+            break;
+
+            case "2":
+            //visualizar
+            //se estiver vazio dar aviso
+                if(funcionario == null){
+                    JOptionPane.showMessageDialog(null, 
+                                "Precisa cadastrar os dados",
+                                "Alerta",
+                                JOptionPane.ERROR_MESSAGE);
+            break;
+                }
+            
+            JOptionPane.showMessageDialog(null, funcionario.imprimir());
+                break;
+
+            case "3":
+            //alterar remuneração
+            //eceber novamente salário ou valor hora e qtde hora dependendo do tipo vínculo
+            break;
+            
+            case "4":
+            //sair
+            break;
+
+            default:
+        }        
+
+    }while(!item.equals("3"));
+}}
+
+
 }
--- POO/LISTA1/exercicio3/funcionario.java
+++ POO/LISTA1/exercicio3/funcionario.java
@@ -1,5 +1,81 @@
 package POO.LISTA1.exercicio3;
 
-public class funcionario {
-    
+public class Funcionario {
+    private String nome;
+    private float valorHora, qntdHora, salario, valorDesconto, valorReceber;
+    int cracha;
+    int tipoVinculo; //deveria ser char H ou N
+
+        public String getnome(){
+            return nome;
+        }
+        public void setnome(String value){
+            this.nome = value;
+        }
+        public float getvalorHora(){
+            return valorHora;
+        }
+        public void setvalorHora(float value){
+            this.valorHora = value;
+        }
+        public float getqntdHora(){
+            return qntdHora;
+        }
+        public void setqntdHora(float value){
+            this.qntdHora = value;
+        }
+        public float getsalario(){
+            return salario;
+        }
+        public void setsalario(float value){
+            this.salario = value;
+        }
+        public float getvalorDesconto(){
+            return valorDesconto;
+        }
+        public void setvalorDesconto(float value){
+            this.valorDesconto = value;
+        }
+        public int getcracha(){
+            return cracha;
+        }
+        public void setcracha(int value){
+            this.cracha = value;
+        }
+        public int gettipoVinculo(){
+            return tipoVinculo;
+        }
+        public void settipoVinculo(int value){
+            this.tipoVinculo = value;
+        }     
+        public float getvalorReceber(){
+            return valorReceber;
+        }
+        public void setvalorReceber(float value){
+            this.valorReceber = value;
+        } 
+
+        public String imprimir(){
+            String msg = "\n===================" 
+                +"\nCrachá: "+getcracha()
+                +"\nNome: "+getnome()
+                +"\nTipo Vínculo: "+gettipoVinculo()
+                +"\nSalário: "+getsalario()
+                +"\nDesconto: "+getvalorDesconto()
+                +"\nValor a receber: "+getvalorReceber();
+        
+        return msg;    
+        }
+
+        public float calcularSalario(){
+           valorReceber = valorHora * qntdHora;
+            return valorReceber;
+        }
+
+        public float calcularValorReceber(){
+            valorReceber = salario - valorDesconto;
+            return valorReceber;
+            
+        }
+
 }
