32b5c2c9b17c5ec453ac1cb2667bf2725ecba8e8
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-08-23 22:11:02 -0300

    lista2 ex1

---
+++ POO/aulaConstrutor/LISTA2/ex1/Apolice.java
@@ -0,0 +1,83 @@
+package POO.aulaConstrutor.LISTA2.ex1;
+
+public class Apolice {
+    private int numero, idade;
+    private String nome;
+    private double valorAutomovel, valorApolice;
+    private char sexo;
+    
+
+    public Apolice(){
+
+    }
+
+    public Apolice(int numero, String nome, int idade, char sexo, double valorAutomovel){
+        this.numero = numero;
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.valorAutomovel = valorAutomovel;
+    }
+    
+    public String imprimir(){
+        return "\nNúmero: " + getNumero()
+                +"\nNome: " + getNome()
+                +"\nIdade: " + getIdade()
+                +"\nSexo: " + getSexo()
+                +"\nValor automóvel: " + getValorAutomovel()
+                +"\n-----------------------------"
+                +"\nValor apolice: " + getValorApolice();
+    }
+
+    public double calcularApolice(){
+        if (sexo == 'M' && idade <= 25){
+        valorApolice = valorAutomovel * 10/100;
+        return valorApolice;    
+        } 
+        else if (sexo == 'M' && idade >25){
+        valorApolice = valorAutomovel * 5/10;
+        return valorApolice;    
+        }
+        else {
+        valorApolice = valorAutomovel * 2/100;
+        return valorApolice;
+        }
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+    public double getValorAutomovel() {
+        return valorAutomovel;
+    }
+    public void setValorAutomovel(double valorAutomovel) {
+        this.valorAutomovel = valorAutomovel;
+    }
+    public double getValorApolice() {
+        return calcularApolice();
+    }
+    public void setValorApolice(double valorApolice) {
+        this.valorApolice = valorApolice;
+    }
+}
