a6cd2a146ab8e44c1164ce0073a97add696e6ec5
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-10-09 10:35:27 -0300

    EXERCICIO

--- Bando_Dados/Lista_Delete.Alter.Update.Drop/sql_animais.sql
+++ Bando_Dados/Lista_Delete.Alter.Update.Drop/sql_animais.sql
@@ -6,6 +6,10 @@ CREATE TABLE Animais2 (
   	cor varchar(50)
 );
 
+CREATE TABLE Especies (
+especie varchar(30)
+);
+
 INSERT INTO Animais2 values (01 , 'Ágata' , '2015-04-09' , 13.9 , 'branco' );
 INSERT INTO Animais2 values (02 , 'Félix' , '2016-06-06' , 14.3 , 'preto' );
 INSERT INTO Animais2 values (03 , 'Tom' , '2013-02-08' , 11.2 , 'azul' );
@@ -29,3 +33,35 @@ INSERT INTO Animais2 values (24 , 'Rufus' , '2014-04-05' , 19.7 , 'branco' );
 INSERT INTO Animais2 values (25 , 'Rex' , '2021-08-19' , 19.7 , 'branco' );
 
 SELECT * FROM Animais2;
+
+/* Altere o nome do Pateta para Goofy; */
+UPDATE Animais2 SET nome = 'Goofy' WHERE id = 15;
+SELECT * FROM Animais2 WHERE id = 15;
+
+/* Altere o peso do Garfield para 10 quilogramas; */
+UPDATE Animais2 SET peso=10 WHERE id = 4;
+SELECT * FROM Animais2 WHERE id = 4;
+
+/* Altere a cor de todos os gatos para laranja; */
+UPDATE Animais2 SET cor='laranja' WHERE 
+SELECT * FROM Animais2;
+
+/* Crie um campo altura para os animais; */
+
+/* Crie um campo observação para os animais; */
+
+/* Remova todos os animais que pesam mais que 200 quilogramas. */
+
+/* Remova todos os animais que o nome inicie com a letra ‘C’. */
+
+/* Remova o campo cor dos animais; */
+
+/* Aumente o tamanho do campo nome dos animais para 80 caracteres; */
+
+/* Remova todos os gatos e cachorros. */
+
+/* Remova o campo data de nascimento dos animais. */
+
+/* Remova todos os animais. */
+
+/* Remova a tabela especies. */
