c727148a6d280c945793dcb07ba4d51341216449
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-10-09 09:52:34 -0300

    sql

---
+++ Bando_Dados/Lista_Delete.Alter.Update.Drop/README.md
@@ -0,0 +1,35 @@
+<h1>Proposta:</h1>
+Foi proposto a criação de duas tabelas com relacionamento entre si:
+
+- Cidades: id, nome e população.
+- Alunos: id, nome, data de nascimento e cidade.
+
+Depois foram inseridos dados, sendo: 5 cidades e 12 alunos.
+
+Após a criação das tabelas foi lançado o desafio para criar Inner Join e Left-Right Join.
+
+O execício foi elaborado através do [SQL Server da Oracle](https://dev.mysql.com/doc/) e [WAMP](https://sourceforge.net/projects/wampserver/).
+
+O <b>código completo</b> desenvolvido para a resolução do exercício está disponibilizado [aqui](https://github.com/thaisconto/Curso-ADS/blob/main/Bando_Dados/Lista_InnerJoin/sql_ex2_aula7.sql).
+
+
+------------------------------------------------
+- Fazendo uma consulta juntando as tabelas alunos com suas referentes cidades, de duas formas:
+
+   Inner Join
+<img src = innerjoin.png>
+
+
+Join on
+
+<img src = joinon.png>
+
+ -------------------------------------------------------
+- Usando left join para deixar na ordem do id dos alunos;
+<img src = leftjoin.png>
+
+ -------------------------------------------------------
+- Usando right join para deixar na ordem do id das cidades;
+<img src = rightjoin.png>
+ 
+  
---
+++ Bando_Dados/Lista_Delete.Alter.Update.Drop/sql_animais.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Animais2 (
+	id int,
+ 	nome varchar(50),
+  	nasc date,
+  	peso decimal(10,2),
+  	cor varchar(50)
+);
+
+INSERT INTO Animais2 values (01 , 'Ágata' , '2015-04-09' , 13.9 , 'branco' );
+INSERT INTO Animais2 values (02 , 'Félix' , '2016-06-06' , 14.3 , 'preto' );
+INSERT INTO Animais2 values (03 , 'Tom' , '2013-02-08' , 11.2 , 'azul' );
+INSERT INTO Animais2 values (04 , 'Garfield' , '2015-07-06' , 17.1 , 'laranja' );
+INSERT INTO Animais2 values (05 , 'Frajola' , '2013-08-01' , 13.7 , 'preto' );
+INSERT INTO Animais2 values (06 , 'Manda-chuva' , '2012-02-03' , 12.3 , 'amarelo' );
+INSERT INTO Animais2 values (07 , 'Snowball' , '2014-04-06' , 13.2 , 'amarelo' );
+INSERT INTO Animais2 values (10 , 'Ágata' , '2015-08-03' , 11.9 , 'azul' );
+INSERT INTO Animais2 values (11 , 'Gato de botas' , '2012-12-10' , 11.6 , 'amarelo' );
+INSERT INTO Animais2 values (12 , 'Kitty' , '2020-04-06' , 11.6 , 'amarelo' );
+INSERT INTO Animais2 values (13 , 'Milu' , '2013-02-04' , 17.9 , 'branco' );
+INSERT INTO Animais2 values (14 , 'Pluto' , '2012-01-03' , 12.3 , 'amarelo' );
+INSERT INTO Animais2 values (15 , 'Pateta' ,'2015-05-01' , 17.7 , 'preto' );
+INSERT INTO Animais2 values (16 , 'Snoopy' , '2013-07-02' , 18.2 , 'branco' );
+INSERT INTO Animais2 values (17 , 'Rex' , '2019-11-03' , 19.7 , 'beje' );
+INSERT INTO Animais2 values (20 , 'Bidu' , '2012-09-08' , 12.4 , 'azul' );
+INSERT INTO Animais2 values (21 , 'Dum Dum' , '2015-04-06' , 11.2 , 'laranja' );
+INSERT INTO Animais2 values (22 , 'Muttley' , '2011-02-03' , 14.3 , 'laranja' );
+INSERT INTO Animais2 values (23 , 'Scooby' , '2012-01-02' , 19.9 , 'marrom' );
+INSERT INTO Animais2 values (24 , 'Rufus' , '2014-04-05' , 19.7 , 'branco' );
+INSERT INTO Animais2 values (25 , 'Rex' , '2021-08-19' , 19.7 , 'branco' );
+
+SELECT * FROM Animais2;
