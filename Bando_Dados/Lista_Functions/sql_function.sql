d63bbbf21dd1e1b3e100d617dd9ca0b3ac4fff3b
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-10-31 11:14:51 -0300

    lista_ger_pedidos_bd

---
+++ Bando_Dados/Lista_Functions/sql_function.sql
@@ -0,0 +1,14 @@
+/*
+Crie um banco de dados para armazenar alunos e cursos de uma universidade;
+• Cada curso pode pertencer a somente uma área;
+• Utilize Stored Procedures para automatizar a inserção e seleção dos cursos;
+• O aluno possui um e-mail que deve ter seu endereço gerado automaticamente no
+seguinte formato: nome.sobrenome@dominio.com
+• Crie uma rotina que recebe os dados de um novo curso e o insere no banco de dados;
+• Crie uma função que recebe o nome de um curso e sua área, em seguida retorna o id do
+curso;
+• Crie uma procedure que recebe os dados do aluno e de um curso e faz sua matrícula;
+• Caso o aluno já esteja matriculado em um curso, essa matrícula não pode ser realizada;
+• Crie o modelo lógico do exercício.
+
+*/
--- Bando_Dados/Lista_Gerenciamento_Pedidos/sql.sql
+++ Bando_Dados/Lista_Gerenciamento_Pedidos/sql.sql
@@ -1,16 +1,119 @@
 /*
-Atividade: Sistema de Gerenciamento de Pedidos
-Objetivo: Criar um sistema de gerenciamento de pedidos em um banco de dados utilizando stored procedures, triggers, views e JOINs no MySQL Workbench.
-Etapa 1: Criação de Tabelas e Inserção de Dados
-Crie as tabelas "Clientes" e "Pedidos" com campos apropriados. Insira dados de exemplo nas tabelas para simular clientes e pedidos. Certifique-se de incluir uma chave primária em cada tabela.
-Etapa 2: Criação de Stored Procedure
-Crie uma stored procedure chamada "InserirPedido" que permite inserir um novo pedido na tabela "Pedidos" com as informações apropriadas. A stored procedure deve receber parâmetros como o ID do cliente e os detalhes do pedido. Ao término teste o funcionamento da stored procedure criada inserindo um pedido.
-Etapa 3: Trigger
-Crie uma trigger que seja acionada APÓS a inserção de um novo pedido na tabela "Pedidos". A trigger deve calcular o valor total dos pedidos para o cliente correspondente e atualizar um campo "TotalPedidos" na tabela "Clientes" com o valor total. Teste a Trigger inserindo um novo pedido na tabela "Pedidos“.
-Etapa 4: View
-Crie uma view chamada "PedidosClientes" que combina informações das tabelas "Clientes" e "Pedidos" usando JOIN para mostrar os detalhes dos pedidos e os nomes dos clientes.
-Etapa 5: Consulta com JOIN
-Escreva uma consulta SQL que utiliza JOIN para listar os detalhes dos pedidos de cada cliente, incluindo o nome do cliente e o valor total de seus pedidos. Utilize a view "PedidosClientes" para essa consulta.
 */
 CREATE DATABASE gerenciamento_pedidos;
 USE gerenciamento_pedidos;
+
+-- -----------------------------------------------------
+-- Criando as tabelas
+-- -----------------------------------------------------
+
+-- Tabela Clientes
+CREATE TABLE IF NOT EXISTS Clientes (
+  id_cliente INT NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  cpf VARCHAR(45) NOT NULL UNIQUE,
+  total_pedidos DECIMAL(10,2) NULL,
+  PRIMARY KEY (id_cliente)
+ );
+
+-- Tabela Pedidos
+CREATE TABLE IF NOT EXISTS Pedidos (
+  id_pedido INT NOT NULL AUTO_INCREMENT,
+  cliente_id INT NOT NULL,
+  valor DECIMAL(10,2) NOT NULL,
+  detalhe VARCHAR(45) NULL,
+  PRIMARY KEY (id_pedido),
+  FOREIGN KEY (cliente_id) REFERENCES Clientes (id_cliente)
+  );
+
+-- Tabela Clientes_Pedidos
+CREATE TABLE IF NOT EXISTS Clientes_Pedidos (
+  cp_cliente_id INT NOT NULL,
+  cp_pedido_id INT NOT NULL,
+  PRIMARY KEY (cp_cliente_id, cp_pedido_id),
+  FOREIGN KEY (cp_pedido_id) REFERENCES Pedidos (id_pedido),
+  FOREIGN KEY (cp_cliente_id) REFERENCES Clientes (id_cliente)
+);
+
+-- -----------------------------------------------------
+-- Inserindo dados
+-- -----------------------------------------------------
+
+-- clientes
+INSERT INTO Clientes VALUES (null, 'Cliente 1', '1111', null);
+INSERT INTO Clientes VALUES (null, 'Cliente 2', '2222', null);
+INSERT INTO Clientes VALUES (null, 'Cliente 3', '3333', null);
+
+select * from Clientes;
+
+-- pedidos
+INSERT INTO Pedidos VALUES (null, 1, 10.99, 'detalhe 1');
+INSERT INTO Pedidos VALUES (null, 2, 20.99, 'detalhe 2');
+INSERT INTO Pedidos VALUES (null, 3, 30.99, 'detalhe 3');
+
+select * from Pedidos;
+
+-- -----------------------------------------------------
+-- Criação de Stored Procedure
+-- -----------------------------------------------------
+-- inserir um novo pedido na tabela 
+
+delimiter $
+create procedure InserirPedido (
+  cliente_id INT,
+  valor DECIMAL (10,2),
+  detalhe VARCHAR(45)
+)
+BEGIN
+	  INSERT INTO Pedidos (cliente_id, valor, detalhe) VALUES (cliente_id, valor, detalhe);
+END$
+DELIMITER ;
+
+call InserirPedido (1, 1.99, 'detalhe stored procedure');
+select * from pedidos;
+
+-- -----------------------------------------------------
+-- Criação Trigger
+-- -----------------------------------------------------
+-- acionada APÓS a inserção de um novo pedido. Calcular o valor total dos pedidos para o cliente 
+DELIMITER $
+CREATE TRIGGER AtualizarTotalPedidosCliente
+	AFTER INSERT ON Pedidos  
+    FOR EACH ROW
+    BEGIN
+			UPDATE Clientes 
+            SET total_pedidos = (SELECT SUM(valor) 
+								 FROM Pedidos 
+								 WHERE cliente_id = NEW.cliente_id)
+            WHERE Clientes.id_cliente = NEW.cliente_id;
+	END;
+    $
+DELIMITER ;
+
+call InserirPedido (1, 23.99, 'detalhe trigger');
+select * from pedidos;
+select * from Clientes;
+
+-- -----------------------------------------------------
+-- Criação View
+-- -----------------------------------------------------
+-- combina informações usando JOIN para mostrar os detalhes dos pedidos e os nomes dos clientes.
+
+CREATE VIEW PedidosClientes as
+SELECT Pedidos.cliente_id, Clientes.nome, Pedidos.detalhe, Pedidos.valor
+FROM Pedidos
+INNER JOIN Clientes
+ON cliente_id = id_cliente;
+
+SELECT * FROM PedidosClientes;
+
+-- -----------------------------------------------------
+-- Criação Consulta com JOIN
+-- -----------------------------------------------------
+-- listar os detalhes dos pedidos de cada cliente, utilize a view "PedidosClientes" para essa consulta.
+
+SELECT  PedidosClientes.nome, PedidosClientes.valor, PedidosClientes.detalhe, 
+		Clientes.total_pedidos
+FROM PedidosClientes
+JOIN Clientes
+ON PedidosClientes.cliente_id = Clientes.id_cliente;
