22fcb8e7dfd06e1edd6fe7895e2dab09909964ce
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2023-11-13 19:19:04 -0300

    bd

---
+++ Bando_Dados/sql_normalizacao.sql
@@ -0,0 +1,5 @@
+/*
+Observe a tabela não normalizada de uma locadora de veículos e aplique a 3ª Forma normal; 
+Faça o modelo lógico de banco de dados relacional; Escreva o script que cria as tabelas; 
+Crie uma view que seleciona todas as locações e seus respectivos veículos e clientes.
+*/
